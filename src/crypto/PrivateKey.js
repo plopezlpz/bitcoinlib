917504de622c76d3ab38d5220edbb118c7e18bd1
pablo <pablo@chainalysis.com>
2019-10-24 18:47:44 +0300

    added priv key

---
+++ src/crypto/PrivateKey.js
@@ -0,0 +1,59 @@
+const BN = require("bn.js");
+const { G, N } = require("./Point");
+const Signature = require("../Signature");
+const { toBN, toOrderN } = require("../utils/num");
+
+const N2 = new BN(2);
+
+class PrivateKey {
+  /**
+   * @param {string|number|BN} secret
+   */
+  constructor(secret) {
+    this.secret = toBN(secret);
+    this.point = G.sMul(secret);
+  }
+
+  /**
+   * Signs z
+   * @param {string|number|BN} z
+   * @returns {Signature}
+   */
+  sign(z) {
+    // eslint-disable-next-line no-param-reassign
+    z = toBN(z);
+    // It is imperative not to reuse k
+    const k = this.deterministicK(z);
+    /** @type {BN} */
+    const r = G.sMul(k).x;
+
+    const kInv = toOrderN(k).redInvm();
+    // prettier-ignore
+    let s = toOrderN(r.mul(this.secret).add(z)).redMul(kInv);
+    // using low-s value will get nodes to relay our tx (this is for malleability reasons)
+    if (s.lt(N.div(N2))) {
+      s = N.sub(s);
+    }
+    return new Signature(r, s);
+  }
+
+  /**
+   * Returns a deterministic k in the range (0, N], specification RFC 6979
+   * The alternative would be to use a trully random k
+   * @param {string|number|BN} z
+   * @returns {BN}
+   */
+  deterministicK(z) {
+    const v = Buffer.alloc(32);
+    v.fill(0x01);
+    const k = Buffer.alloc(32);
+    k.fill(0x00);
+    if (z.gt(N)) {
+      z = z.minus(N);
+    }
+
+    return new BN(22); // in range [0, N)
+  }
+}
+
+module.exports = PrivateKey;
---
+++ src/crypto/PrivateKey.spec.js
@@ -0,0 +1,21 @@
+const BN = require("bn.js");
+const { stub } = require("sinon");
+const { expect } = require("chai");
+const sha = require("../utils/sha");
+const PrivateKey = require("./PrivateKey");
+
+describe("PrivateKey", () => {
+  it("signs tx (brainwallet style)", () => {
+    const e = new BN(sha("my secret"), "hex");
+    const z = new BN(sha("my message"), "hex");
+    const priv = new PrivateKey(e);
+    stub(priv, "deterministicK").callsFake(() => new BN(1234567890));
+
+    const sig = priv.sign(z);
+
+    // prettier-ignore
+    expect(sig.r.toString(16)).to.be.equal("2b698a0f0a4041b77e63488ad48c23e8e8838dd1fb7520408b121697b782ef22");
+    // prettier-ignore
+    expect(sig.s.toString(16)).to.be.equal("bb14e602ef9e3f872e25fad328466b34e6734b7a0fcd58b1eb635447ffae8cb9");
+  });
+});
