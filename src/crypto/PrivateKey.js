4b3c9ba93bcbf567c9e92fba314dcd2c71e098e5
pablo <pablo@chainalysis.com>
2019-10-26 11:23:53 +0300

    deterministic k

--- src/crypto/PrivateKey.js
+++ src/crypto/PrivateKey.js
@@ -1,7 +1,12 @@
 const BN = require("bn.js");
+const { sha256 } = require("js-sha256");
 const { G, N } = require("./Point");
 const Signature = require("./Signature");
 const { toBN, toOrderN } = require("../utils/num");
+// TODO for react-native:
+// npm install buffer --save
+// import { Buffer } from 'buffer';
+// global.Buffer = Buffer; // very important in your App.js (or use the Buffer only here)
 
 const N2 = new BN(2);
 
@@ -31,6 +36,7 @@ class PrivateKey {
     // prettier-ignore
     let s = toOrderN(r.mul(this.secret).add(z)).redMul(kInv);
     // using low-s value will get nodes to relay our tx (this is for malleability reasons)
+    // see BIP 62, "low S values in signatures"
     if (s.lt(N.div(N2))) {
       s = N.sub(s);
     }
@@ -44,15 +50,37 @@ class PrivateKey {
    * @returns {BN}
    */
   deterministicK(z) {
-    const v = Buffer.alloc(32);
+    let v = Buffer.alloc(32);
     v.fill(0x01);
-    const k = Buffer.alloc(32);
+    let k = Buffer.alloc(32);
     k.fill(0x00);
+
     if (z.gt(N)) {
+      // eslint-disable-next-line no-param-reassign
       z = z.minus(N);
     }
 
-    return new BN(22); // in range [0, N)
+    const secretBuff = this.secret.toArrayLike(Buffer, "be", 32);
+    const zBuff = z.toArrayLike(Buffer, "be", 32);
+    // prettier-ignore
+    k = sha256.hmac.arrayBuffer(k, Buffer.concat([v, Buffer.from([0x00]), secretBuff, zBuff]));
+    v = sha256.hmac.arrayBuffer(k, v);
+    // prettier-ignore
+    k = sha256.hmac.arrayBuffer(k, Buffer.concat([Buffer.from(v), Buffer.from([0x01]), secretBuff, zBuff]))
+    v = sha256.hmac.arrayBuffer(k, v);
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      v = sha256.hmac.arrayBuffer(k, v);
+      const vBuff = Buffer.from(v);
+      const candidate = toBN(`0x${vBuff.toString("hex")}`);
+      if (candidate.gte(new BN(1)) && candidate.lt(N)) {
+        return candidate;
+      }
+      // prettier-ignore
+      k = sha256.hmac.arrayBuffer(k, Buffer.concat([vBuff, Buffer.from([0x01]), secretBuff, zBuff]))
+      v = sha256.hmac.arrayBuffer(k, v);
+    }
   }
 }
 
--- src/crypto/PrivateKey.spec.js
+++ src/crypto/PrivateKey.spec.js
@@ -18,4 +18,13 @@ describe("PrivateKey", () => {
     // prettier-ignore
     expect(sig.s.toString(16)).to.be.equal("bb14e602ef9e3f872e25fad328466b34e6734b7a0fcd58b1eb635447ffae8cb9");
   });
+
+  it("deterministicK", () => {
+    const e = new BN(sha("my secret"), "hex");
+    const z = new BN(sha("my message"), "hex");
+    const priv = new PrivateKey(e);
+
+    // prettier-ignore
+    expect(priv.deterministicK(z).toString(10)).to.equal("113213234669682963325447032715629850130244745741694426314257695186651662957264");
+  });
 });
